184b857a2368361354a9e78e44fc3fdcb9c7cf6a
Dennis Effa Amponsah <kamponsah.amponsah@gmail.com>
2019-12-04 12:32:38 +0000

    added client service

---
+++ src/app/clientservice.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from "@angular/core";
+import { Client } from "./screens/clients/client";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+
+@Injectable({
+  providedIn: "root"
+})
+export class ClientserviceService {
+  constructor(private http: HttpClient) {}
+  // private clientsUrl = "http://127.0.0.1:5000/";
+  private clientsUrl = "https://boiling-bastion-30474.herokuapp.com/clients";
+
+  getClients(): Observable<Client[]> {
+    return this.http.get<Client[]>(this.clientsUrl);
+  }
+}
--- src/app/screens/clients/clients-screen/clients-screen.component.ts
+++ src/app/screens/clients/clients-screen/clients-screen.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { Client } from "../client";
 import { Observable } from "rxjs";
+import { ClientserviceService } from "src/app/clientservice.service";
 
 @Component({
   selector: "app-clients-screen",
@@ -10,19 +11,24 @@ import { Observable } from "rxjs";
 })
 export class ClientsScreenComponent implements OnInit {
   results = [];
-  constructor(private http: HttpClient) {}
+  constructor(private clientService: ClientserviceService) {}
   customersObservable: Observable<Client[]>;
+  c =[]
   ngOnInit() {
     // this.results = this.http.get<Client>("https://boiling-bastion-30474.herokuapp.com/clients")
     // alert(JSON.stringify(this.getClients()));
-    this.customersObservable = this.http
-      .get<Client[]>("https://boiling-bastion-30474.herokuapp.com/clients").subscribe(d =>console.log(d))
-  }
-  getClients(): Observable<Client[]> {
-    return this.http.get<Client[]>(
-      "https://boiling-bastion-30474.herokuapp.com/clients"
+    // this.getClients()
+    // this.customersObservable = this.http
+    // .get<Client[]>("https://boiling-bastion-30474.herokuapp.com/clients").subscribe(d =>console.log(d))
+    console.log(
+      this.clientService.getClients().subscribe(client => console.log(client))
     );
   }
+  // getClients(): Observable<Client[]> {
+  //   return this.http.get<Client[]>(
+  //     "http://localhost:5050"
+  //   ).subscribe(client =>console.log(client))
+  // }
   clients = [
     { id: 1, name: "Dennis" },
     { id: 2, name: "James" },
