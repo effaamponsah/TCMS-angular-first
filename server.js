f38e675a90e485734f097c5da9e23a2fe322ea04
Dennis Effa Amponsah <kamponsah.amponsah@gmail.com>
2019-12-04 11:14:36 +0000

    changed the app name

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/tcmsUI'));
+app.use(express.static(__dirname + '/dist/tcms-angular'));
 
 app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+'/dist/tcmsUI/index.html'));
+res.sendFile(path.join(__dirname+'/dist/tcms-angular/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
